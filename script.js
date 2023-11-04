8b16cd18588fdc8e63272d5d1d32a540fa1892a1
zaquari_andl <zaquariandl043@gmail.com>
2023-11-04 05:02:09 -0500

    started crafting keyboard from previous version

--- script.js
+++ script.js
@@ -1,15 +1,135 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content is added by JavaScript";
 document.querySelector("body").appendChild(h2);
+document.addEventListener("keydown", function(hightlight) {
+    if(hightlight.key === "Tab") {
+    const TabKey = document.querySelector("#tabKey");
+    TabKey.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(hightlight) {
+    if(hightlight.key === "Tab") {
+    const TabKey = document.querySelector("#tabKey");
+    TabKey.style.background = "lightgrey";
+    }
+});
+document.addEventListener("keydown", function(hightlight) {
+    if(hightlight.key === "z") {
+    const zKey = document.querySelector("#zKey");
+    zKey.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(hightlight) {
+    if(hightlight.key === "z") {
+    const zKey = document.querySelector("#zKey");
+    zKey.style.background = "lightgrey";
+    }
+});
 document.addEventListener("keydown", function(highlight) {
     if(highlight.key === "a") {
-        const square = document.querySelector("#square");
-        square.style.background = "lightblue";
+        const aKey = document.querySelector("#aKey");
+        aKey.style.background = "lightblue";
     }
 });
 document.addEventListener("keyup", function(highlight) {
     if(highlight.key === "a") {
-        const square = document.querySelector("#square");
-        square.style.background = "lightgrey";
+        const aKey = document.querySelector("#aKey");
+        aKey.style.background = "lightgrey";
+    }
+});
+document.addEventListener("keydown", function(highlight) {
+    if(highlight.key === "q") {
+    const qKey = document.querySelector("#qKey");
+    qKey.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(highlight) {
+    if(highlight.key === "q") {
+    const qKey = document.querySelector("#qKey");
+    qKey.style.background = "lightgrey";
+    }
+});
+document.addEventListener("keydown", function(highlight) {
+    if(highlight.key === "w") {
+    const wKey = document.querySelector("#wKey");
+    wKey.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(highlight) {
+    if(highlight.key === "w") {
+    const wKey = document.querySelector("#wKey");
+    wKey.style.background = "lightgrey";
+    }
+});
+document.addEventListener("keydown", function(highlight) {
+    if(highlight.key === "s") {
+    const sKey = document.querySelector("#sKey");
+    sKey.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(highlight) {
+    if(highlight.key === "s") {
+    const sKey = document.querySelector("#sKey");
+    sKey.style.background = "lightgrey";
+    }
+});
+document.addEventListener("keydown", function(highlight) {
+    if(highlight.key === "x") {
+    const xKey = document.querySelector("#xKey");
+    xKey.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(highlight) {
+    if(highlight.key === "x") {
+    const xKey = document.querySelector("#xKey");
+    xKey.style.background = "lightgrey";
+    }
+});
+document.addEventListener("keydown", function(highlight) {
+    if(highlight.key === "e") {
+        const eKey = document.querySelector("#eKey");
+        eKey.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(highlight) {
+    if(highlight.key === "e") {
+        const eKey = document.querySelector("#eKey");
+        eKey.style.background = "lightgrey";
+    }
+});
+document.addEventListener("keydown", function(highlight) {
+    if(highlight.key === "d") {
+    const dKey = document.querySelector("#dKey");
+    dKey.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(highlight) {
+    if(highlight.key === "d") {
+    const dKey = document.querySelector("#dKey");
+    dKey.style.background = "lightgrey";
+    }
+});
+document.addEventListener("keydown", function(highlight) {
+    if(highlight.key === "c") {
+    const cKey = document.querySelector("#cKey");
+    cKey.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(highlight) {
+    if(highlight.key === "c") {
+    const cKey = document.querySelector("#cKey");
+    cKey.style.background = "lightgrey";
+    }
+});
+document.addEventListener("keydown", function(highlight) {
+    if(highlight.key === "") {
+    const Key = document.querySelector("#Key");
+    Key.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(highlight) {
+    if(highlight.key === "") {
+    const Key = document.querySelector("#Key");
+    Key.style.background = "lightgrey";
     }
 });
