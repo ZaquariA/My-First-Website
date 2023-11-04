c84141907cba142cb17ff70c6092fa889de0aa77
zaquari_andl <zaquariandl043@gmail.com>
2023-11-04 02:03:55 -0500

    removing keyboard, creating a keypress for the letter a, and attempting to make a working iframe

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content is added by JavaScript";
 document.querySelector("body").appendChild(h2);
+document.addEventListener("keydown", function(highlight) {
+    if(highlight.key === "a") {
+        const square = document.querySelector("#square");
+        square.style.background = "lightblue";
+    }
+});
+document.addEventListener("keyup", function(highlight) {
+    if(highlight.key === "a") {
+        const square = document.querySelector("#square");
+        square.style.background = "lightgrey";
+    }
+});
